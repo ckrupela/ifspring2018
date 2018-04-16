787fad45529aeabfd1bbeba94512419a913613f2
ckrupela <35510982+ckrupela@users.noreply.github.com>
2018-04-16 17:00:37 -0500

    Add files via upload

---
+++ microsite/www/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	$("#landing").click(function(){
+		$(this).fadeOut([.5]);
+		 $(this).hide([.5]);
+	})
+
+	// $("#life").hover(function(){
+	// 	$("#redlife").show();
+	// 	$("#graylife").show();
+
+	})
+
+	// var elem = document.querySelector('.carousel');
+ //    var instance = M.Carousel.init(elem, options);
+
+  // Or with jQuery
+
+     $(".carousel").carousel();
+	
+
+
+
+
+});
